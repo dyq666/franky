758de7d221b7f4c22a871d6b36c3584060e02811
dyq666 <694526892@qq.com>
2020-01-22 15:43:01 +0800

    feature: add float specifiers and float in bases format

--- format_specifier.c
+++ format_specifier.c
@@ -4,17 +4,25 @@
 
 void integer_in_bases(void);
 void char_in_bases(void);
+void float_in_bases(void);
 void integer_specifiers(void);
 void incorrect_integer_specifiers(void);
+void float_specifiers(void);
 
 int main(void) {
     integer_in_bases();
     printf("\n");
     char_in_bases();
     printf("\n");
+    float_in_bases();
+    printf("\n");
+
     integer_specifiers();
     printf("\n");
     incorrect_integer_specifiers();
+    printf("\n");
+
+    float_specifiers();
     printf("\n");
 
     return 0;
@@ -83,4 +91,32 @@ void incorrect_integer_specifiers(void) {
            "When use short type, you can use %%d (%d) or %%hd (%hd)\n",
            overflow, overflow,
            small, small);
+}
+
+void float_in_bases(void) {
+    double dec = 3.2E-2;
+    // f = 15, 0.1 = 1 / 16 = 0.0625, p10 = 2 ** 10, 15.0625 * (2 ** 10) == 15424.0
+    double hex = 0xf.1p10;
+
+    printf("The value of `3.2E-2` is %f\n"
+           "The value of `0xf.0p10` is %f\n"
+           "The exponential representation of `3.2E-2` is %e\n"
+           "The hexadecimal exponential representation of `3.2E-2` is %a\n",
+           dec,
+           hex,
+           dec,
+           dec);
+}
+
+void float_specifiers(void) {
+    float float_ = 2.3f;
+    double double_float = 2.3;
+    long double long_double_float = 2.3L;
+
+    printf("float use %%f (%f)\n"
+           "double use %%f (%f)\n"
+           "long double use %%Lf (%Lf)\n",
+           float_,
+           double_float,
+           long_double_float);
 }
