5bd5253e5b9b63de3f30b7961d3c21f282b679fc
dyq666 <694526892@qq.com>
2020-02-13 10:10:31 +0800

    feature: add util `get_int`

--- util.c
+++ util.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void discard_chars(void);
+int get_int(void);
 
 int main(void){
     return 0;
@@ -12,3 +13,20 @@ void discard_chars(void) {
         ;
     }
 }
+
+int get_int(void) {
+    /* 读取一个整数, 如果错误, 则让用户重复输入. 
+    可以作为读取其他类型的参考, 需要注意的是在使用 `scanf` 后, 要合理的清除 stream 中剩余的字符.    
+    */
+    int input;
+
+    printf("Please enter an integer: ");
+    while (scanf("%d", &input) != 1) {
+        discard_chars();
+        printf("Please enter an integer: ");
+    }
+    discard_chars();
+
+    return input;
+}
+
